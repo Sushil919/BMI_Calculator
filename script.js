6cc5748a834a0f00f7187071c69b5788e029ed32
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2024-01-06 22:48:30 +0530

    Add files via upload
    Calculate BMI

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+//this usecase will provide you empty value
+// const height = parseInt(document.querySelector('#height').value);
+
+document.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#result');
+
+    if (height='' || height < 0 || isNaN(height)){
+        results.innerHTML = 'Please give a valid height ${height}';
+    }
+    else if (weight='' || weight < 0 || isNaN(weight)){
+        results.innerHTML = 'Please give a valid weight ${weight}';
+    }
+    else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+        //shows the result
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+});
